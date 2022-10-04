a89192f101c3fbae5f8c7ded5f1ce48d5690a757
daye <sondayae@gmail.com>
2022-10-04 15:56:54 +0900

    first commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+function Card(){
+    return(
+        <div className="card">
+            <div className="card-inner">
+                <div className="card-front">
+                    <h2>Card Front</h2>
+                </div>
+                <div className="card-back">
+                    <h2>Card Back</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
