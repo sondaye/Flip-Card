03f86df07d9633c1af53347f5e5064fc7cd63a14
daye <sondayae@gmail.com>
2022-10-04 16:28:05 +0900

    hover - flip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Card from './Card';
+/**@jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
+
+const BodyStyle = css`
+  width: 100%;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" css={BodyStyle}>
       <Card />
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,79 @@
+/**@jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
+
+const CardStyle = css`
+    margin: 100px auto 0;
+    width: 400px;
+    height: 600px;
+    perspective: 1000px;
+`;
+
+const CardInner = css`
+    width: 100%;
+    height: 100%;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+    cursor: pointer;
+    position: relative;
+    border: 1px solid black;
+    &:hover{
+        transform: rotateY(180deg);
+    }
+`;
+
+const CardFace = css`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    overflow: hidden;
+    box-shadow: 0px 3px 18px rgba(0,0,0,0.2);
+`;
+
+const CardFront = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: green;
+`;
+
+const CardBack = css`
+    background: red;
+    transform: rotateY(180deg);
+`;
+
+const cardHeader = css`
+    position: reative;
+    padding: 30px 30px 40px;
+    &:after{
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        z-index: -1;
+    }
+`;
+
 function Card(){
     return(
-        <div className="card">
-            <div className="card-inner">
-                <div className="card-front">
+        <div className="card" css={CardStyle}>
+            <div className="card-inner" css={CardInner}>
+                <div className="card-front" css={[CardFace, CardFront]}>
                     <h2>Card Front</h2>
                 </div>
-                <div className="card-back">
-                    <h2>Card Back</h2>
+                <div className="card-back" css={[CardFace, CardBack]}>
+                    <div className="card-content">
+                        <div className="card-header" css={cardHeader}>
+                            <h3>Card Back title</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
